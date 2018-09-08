fcf1f9234c5e99bf8a89feeeb99272978ecf5e10
sbx_user1059 <sbx_user1059@ip-10-128-43-131.eu-west-2.compute.internal>
2018-09-08 12:51:17 +0000

    Sat Sep 08 12:51:17 UTC 2018

--- src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
+++ src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
@@ -7,17 +7,19 @@ public class FizzBuzzSolution {
         StringBuilder builder = new StringBuilder();
 
         String result = String.valueOf(number);
-            if (number > 10 && result.matches("[1-9]")) {
-                wrapper(builder, "deluxe");
-            } else if (result.contains("5") || number % 5 == 0) {
-                wrapper(builder, "buzz");
-            } else if (result.contains("3") ||number % 3 == 0) {
-                wrapper(builder, "fizz");
-            } else {
-                wrapper(builder, String.valueOf(number));
-            }
+        if (number > 10 && result.matches("[1-9]")) {
+            wrapper(builder, "deluxe");
+        } else if ((result.contains("5") || number % 5 == 0) &&
+            (result.contains("3") ||number % 3 == 0)) {
+            wrapper(builder, "fizz buzz");
+        } else if (result.contains("5") || number % 5 == 0) {
+            wrapper(builder, "buzz");
+        } else if (result.contains("3") ||number % 3 == 0) {
+            wrapper(builder, "fizz");
+        } else {
+            wrapper(builder, String.valueOf(number));
+        }
 
-     //   }
         return builder.toString();
     }
 
