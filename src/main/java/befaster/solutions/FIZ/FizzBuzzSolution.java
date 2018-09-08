9c7a14ad066d95e1f0e84c80f20ba12763a980f8
sbx_user1059 <sbx_user1059@ip-10-128-43-131.eu-west-2.compute.internal>
2018-09-08 13:08:14 +0000

    Sat Sep 08 13:08:14 UTC 2018

--- src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
+++ src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
@@ -7,7 +7,7 @@ public class FizzBuzzSolution {
         StringBuilder builder = new StringBuilder();
 
         String result = String.valueOf(number);
-        if (number > 10 && result.matches("^[1-9]+$")) {
+        if (number > 10 && allSame(result)) {
             wrapper(builder, "deluxe");
         } else if ((result.contains("5") || number % 5 == 0) &&
             (result.contains("3") ||number % 3 == 0)) {
@@ -25,6 +25,17 @@ public class FizzBuzzSolution {
 
     private void wrapper(StringBuilder builder, String textToAdd) {
         builder.append(textToAdd);
+    }
+
+    private boolean allSame(String toCheck) {
+        char first = toCheck.charAt(0);
+        for (int i = 1; i < toCheck.length(); i++) {
+            if (first != toCheck.charAt(i)) {
+                return false;
+            }
+            first = toCheck.charAt(i);
+        }
+        return true;
     }
 
 }
