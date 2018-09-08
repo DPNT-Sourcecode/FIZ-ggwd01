f756eb36504a6b44157c2991555bf1b615656a36
sbx_user1059 <sbx_user1059@ip-10-128-43-131.eu-west-2.compute.internal>
2018-09-08 12:35:22 +0000

    Sat Sep 08 12:35:22 UTC 2018

--- src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
+++ src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
@@ -6,7 +6,7 @@ public class FizzBuzzSolution {
     public String fizzBuzz(Integer number) {
         StringBuilder builder = new StringBuilder();
       //  for (int i = 1; i <= number; i++) {
-            if (number % 15 == 0) {
+            if ((number % 5 == 0) && (number % 3 == 0)) {
                 wrapper(builder, "fizzbuzz");
             } else if (number % 5 == 0) {
                 wrapper(builder, "buzz");
